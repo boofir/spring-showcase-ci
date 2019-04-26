553eb768f38d76d69dee6da3124e1c7097d5fbfe
ofir b <bnofir@gmail.com>
2019-04-26 12:05:50 +0300

    fixing travis script

--- docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-version=`cat version`
-echo "Version: $version"
-docker tag ci-spring-showcase $DOCKER_USERNAME/ci-spring-showcase:$version
-docker push $DOCKER_USERNAME/ci-spring-showcase 
